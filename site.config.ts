b3268a5178d39093baeaf6edcd99b3e321b8db04
wdan0163 <112330801+wdan0163@users.noreply.github.com>
2022-09-04 00:35:24 +0800

    09/04
    09/04

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '6f7823928a184b16913f7ceb9b1f1fbb',
+  rootNotionPageId: 'eb31e595f77f4004a7c365aa69bd9b81',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'wdan',
-  domain: 'https://cyan-lupin-08a.notion.site/Homepage-c8602e50f69041dfa2c93898f6fe0bc9-16b9784daaee4c62880d4444daddf8a7',
+  domain: 'https://cyan-lupin-08a.notion.site/Quick-Note-eb31e595f77f4004a7c365aa69bd9b81',
   author: 'wdan',
 
   // open graph metadata (optional)
